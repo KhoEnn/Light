06f2b8804795f3d92966f5f017557b9e388a0ddb
KhoEn <42840373+KhoEnn@users.noreply.github.com>
2018-09-27 21:02:13 +0200

    Delete ready.js

--- .gitignore/events/ready.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client) => {
-  console.log(`OMG COUCOU`);
-  client.user.setActivity(`&help | ${client.users.size} utilisateurs | ${client.guilds.size} serveur`, {type: "LISTENING"});
-  /*client.user.setActivity(` &help | ${client.users.size} utilisateurs | ${client.guilds.size} serveur`, {type: "LISTENING"});*/
-  client.user.setStatus("dnd");
-} 
